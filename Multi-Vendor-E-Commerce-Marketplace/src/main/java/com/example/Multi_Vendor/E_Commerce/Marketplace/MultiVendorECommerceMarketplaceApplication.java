290192c3e7f8237a671859611d8f7a11d53a967d
mdAkhtar <afjalali737965@gmail.com>
2026-06-20 17:35:39 +0530

    Configuration Setting

---
+++ Multi-Vendor-E-Commerce-Marketplace/src/main/java/com/example/Multi_Vendor/E_Commerce/Marketplace/MultiVendorECommerceMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Multi_Vendor.E_Commerce.Marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiVendorECommerceMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiVendorECommerceMarketplaceApplication.class, args);
+	}
+
+}
---
+++ Multi-Vendor-E-Commerce-Marketplace/src/test/java/com/example/Multi_Vendor/E_Commerce/Marketplace/MultiVendorECommerceMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Multi_Vendor.E_Commerce.Marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiVendorECommerceMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
